08152bd61fe394e9f86fd17b31e624c17722f44f
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-11-16 21:11:24 +0000

    putchar_fd

--- libft.h
+++ libft.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int ft_isalpha(int c);
 int ft_isdigit(int c);
@@ -35,5 +36,6 @@ char **ft_split(char const *s, char c);
 char *ft_itoa(int n);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char *));
+void ft_putchar_fd(char c, int fd);
 
 #endif /* LIBFT_H */
