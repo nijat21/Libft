39e02f5f37dab1fe4331b46f9073471124f881db
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-11-18 10:06:21 +0000

    lstadd_Front

--- tests/test_lstadd_front.c
+++ tests/test_lstadd_front.c
@@ -2,5 +2,12 @@
 
 int main()
 {
-    t_list *new_node;
+    t_list *first_node = (t_list *)malloc(sizeof(t_list));
+    t_list *new_node = (t_list *)malloc(sizeof(t_list));
+
+    printf("New node next before: %p\n", new_node->next);
+    printf("First node before: %p\n", first_node);
+    ft_lstadd_front(&first_node, new_node);
+    printf("New node next after: %p\n", new_node->next);
+    printf("First node after: %p\n", first_node);
 }
