78fc4e4be92ff8cc88b3e37ee02215d701c2720e
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2025-08-05 00:43:51 +0100

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,31 +12,25 @@
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len;
-	char	*str;
-	char	*start;
+	char *str;
+	char *start;
 
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = malloc(len);
-	if (!str)
-	{
+	if (!s1 && !s2)
 		return (NULL);
-	}
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
 	start = str;
 	while (*s1)
-	{
-		*str = *s1;
-		str++;
-		s1++;
-	}
+		*str++ = *s1++;
 	while (*s2)
-	{
-		*str = *s2;
-		str++;
-		s2++;
-	}
+		*str++ = *s2++;
 	*str = '\0';
 	return (start);
 }
